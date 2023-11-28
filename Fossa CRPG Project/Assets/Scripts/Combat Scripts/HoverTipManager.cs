9452d91b1955e80ff8ed5c3549b413f3aee5b99f
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2023-11-28 18:41:57 +0000

    Trying to fix the tooltips will need to research why they are moving like this

--- Fossa CRPG Project/Assets/Scripts/Combat Scripts/HoverTipManager.cs
+++ Fossa CRPG Project/Assets/Scripts/Combat Scripts/HoverTipManager.cs
@@ -8,6 +8,7 @@ public class HoverTipManager : MonoBehaviour
 {
     public TMP_Text tipText;
     public RectTransform tipWindow;
+    public float tipOffset = 5;
 
     public static Action<string, Vector2> OnMouseHover;
     public static Action OnMouseLoseFocus;
@@ -23,7 +24,7 @@ public class HoverTipManager : MonoBehaviour
         tipText.text = tip;
         tipWindow.gameObject.SetActive(true);
         tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 300 ? 300 : tipText.preferredWidth, tipText.preferredHeight + 5);
-        tipWindow.transform.position = new Vector2(mousePos.x, mousePos.y);
+        tipWindow.transform.position = new Vector2(mousePos.x + (tipWindow.sizeDelta.x) + tipOffset, mousePos.y);
     }
 
     public void HideTip()
