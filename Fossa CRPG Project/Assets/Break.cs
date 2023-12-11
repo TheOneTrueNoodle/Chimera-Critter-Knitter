86cfcde19d08c520dc9f18d64b3fd80058a0d9b1
AoifeBerry <71941986+AoifeBerry@users.noreply.github.com>
2023-12-11 19:32:04 +0000

    StoolBreaking

---
+++ Fossa CRPG Project/Assets/Break.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+
+    public GameObject Stool1;
+    public GameObject Stool2;
+    public GameObject StoolFull;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Stool1.SetActive(false);
+            Stool2.SetActive(true);
+            StartCoroutine(ExampleCoroutine());
+
+        }
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+       
+        yield return new WaitForSeconds(4);
+        Destroy(StoolFull);
+
+    }
+
+}
