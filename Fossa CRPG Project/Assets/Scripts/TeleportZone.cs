19b8225117c1d70b49e497fb7ad9f757856536b3
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2024-02-26 15:36:42 +0000

    Added some simple polish to the teleport zones

--- Fossa CRPG Project/Assets/Scripts/TeleportZone.cs
+++ Fossa CRPG Project/Assets/Scripts/TeleportZone.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class TeleportZone : MonoBehaviour
 {
     public Transform targetPosition;
+    public Animator anim;
 
     public void Call()
+    {
+        anim.Play("Transition");
+    }
+
+    public void Teleport()
     {
         GameObject Oscar = FindObjectOfType<PlayerMovement>().gameObject;
         Oscar.transform.position = targetPosition.position;
