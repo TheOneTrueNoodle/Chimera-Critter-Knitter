c4d96a1a04afcbde3d3ad72315b5bbcb86084975
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2024-02-26 14:55:45 +0000

    Added a couple extra dialogues

---
+++ Fossa CRPG Project/Assets/Scripts/Unique Scripts/Tutorial House/OpenGate_TutorialHouseArea.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate_TutorialHouseArea : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject interactionTrigger;
+    [SerializeField] private DialogueZoneTrigger dontHaveKeyDialogue;
+    [SerializeField] private DialogueZoneTrigger notInfectedDialogue;
+
+    public void Call()
+    {
+        if (AreaManager.current.GetComponent<TutorialHouseArea>().areaBools.ContainsKey("Has gate key") && AreaManager.current.GetComponent<TutorialHouseArea>().areaBools["Has gate key"] == true)
+        {
+            //Has the gate key, so open
+            anim.enabled = true;
+            interactionTrigger.SetActive(false);
+        }
+        else if(AreaManager.current.GetComponent<TutorialHouseArea>().areaBools.ContainsKey("Infected") && AreaManager.current.GetComponent<TutorialHouseArea>().areaBools["Infected"] == true)
+        {
+            //Dialogue for needing the gate key if infected
+            dontHaveKeyDialogue.DialogueButtonTrigger();
+        }
+        else
+        {
+            //Woof?
+            notInfectedDialogue.DialogueButtonTrigger();
+        }
+    }
+}
