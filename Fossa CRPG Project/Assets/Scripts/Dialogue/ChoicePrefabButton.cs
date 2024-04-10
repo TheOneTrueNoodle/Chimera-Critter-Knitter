35d1ee6c53d894c2d41162fa7dd65be66541513c
ctrlcookie <regardingquiet@gmail.com>
2024-04-10 21:38:26 +0100

    Smal fixes

--- Fossa CRPG Project/Assets/Scripts/Dialogue/ChoicePrefabButton.cs
+++ Fossa CRPG Project/Assets/Scripts/Dialogue/ChoicePrefabButton.cs
@@ -50,12 +50,14 @@ public class ChoicePrefabButton : MonoBehaviour
 
     public void fontUp()
     {
-        this.transform.Find("ChoiceText").GetComponent<TextMeshProUGUI>().fontSize = 20;
+        //this.transform.Find("ChoiceText").GetComponent<TextMeshProUGUI>().fontSize = 20; old version
+        GetComponent<TextMeshProUGUI>().fontSize = 20;
     }
 
     public void fontDown()
     {
-        this.transform.Find("ChoiceText").GetComponent<TextMeshProUGUI>().fontSize = 22;
+        //this.transform.Find("ChoiceText").GetComponent<TextMeshProUGUI>().fontSize = 22; old version
+        GetComponent<TextMeshProUGUI>().fontSize = 22;
     }
 
     public void taskOnClick()
