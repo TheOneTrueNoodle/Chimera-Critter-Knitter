fe1c7497d126755421e604ac7dcd5432610e571d
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2024-04-23 16:53:40 +0100

    Changed combat UI

--- Fossa CRPG Project/Assets/Scripts/Combat Scripts/UI Scripts/CombatActionButtonVisuals.cs
+++ Fossa CRPG Project/Assets/Scripts/Combat Scripts/UI Scripts/CombatActionButtonVisuals.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CombatActionButtonVisuals : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class CombatActionButtonVisuals : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Transform selectedParent;
     [SerializeField] private Transform defaultParent;
 
-    public void OnSelect(BaseEventData eventData)
+    [SerializeField] private GameObject selectedVisuals;
+    [SerializeField] private GameObject defaultVisuals;
+
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        gameObject.transform.parent = selectedParent;
+        gameObject.transform.SetParent(selectedParent);
+        selectedVisuals.SetActive(true);
+        defaultVisuals.SetActive(false);
     }
 
-    public void OnDeselect(BaseEventData eventData)
+    public void OnPointerExit(PointerEventData eventData)
     {
-        gameObject.transform.parent = defaultParent;
+        gameObject.transform.SetParent(defaultParent);
+        selectedVisuals.SetActive(false);
+        defaultVisuals.SetActive(true);
     }
 }
